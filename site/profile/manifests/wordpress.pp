9522ce5380f8f67707eb018241aa4952c54323f2
Shobhna Shastri <shobhna@puppet.com>
2017-02-09 17:20:54 -0800

    Removed settings

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -11,8 +11,6 @@ class { 'wordpress':
   db_user     => 'wordpress',
   db_password => 'anothersecretpassword',
   install_dir => '/var/www/html',
-  wp_site_domain => '54.187.92.27',
-  wp_multisite => true,
 }
 
 
