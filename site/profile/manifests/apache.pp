716bb48c68f1c77fa16a99bf72c7fb114aaa28de
Shobhna Shastri <shobhna@puppet.com>
2017-02-09 17:44:56 -0800

    Updating the ghost

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,7 +3,9 @@ class profile::apache {
   class { 'apache': }
   include apache::mod::php
   
-  apache::vhost { 'safewaybloggo.com' :
-    docroot => '/var/www/html',
-  }
+  apache::vhost { $facts['fqdn']:
+        port     => '80',
+        priority => '00',
+        docroot  => '/var/www/html',
+    }
 }
